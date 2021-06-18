ffc26d0a0169b0fbdb7606ce0754714d86ee5f54
alexyan <419725794@qq.com>
2021-06-18 15:57:44 +0800

    refactor

--- src/main/java/org/skoal/restrictor/rule/definition/TrieRefunedRule.java
+++ src/main/java/org/skoal/restrictor/rule/definition/TrieRefunedRule.java
@@ -6,7 +6,7 @@ import org.skoal.restrictor.basic.Trie;
  * 相比hashmap，占用空间较少，耗时较多（约2-5倍的差距）
  */
 public class TrieRefunedRule implements RefinedRule {
-    private Trie<ApiRule> trie = new Trie<>();
+    private final Trie<ApiRule> trie = new Trie<>();
 
     @Override
     public ApiRule getApiRule(String clientId, String api) {
